46b52789018a077fd51a0784769cb700dcaa4ee8
elouannh <elouannhosta27@gmail.com>
2024-09-21 12:50:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Renshuu R(est) Agent
+
+A command line rest agent for the Renshuu app. Get your data and display it directly into your terminal.
+Documentation writing in progress.
---
+++ src/main.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehosta <ehosta@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 11:55:10 by ehosta            #+#    #+#             */
+/*   Updated: 2024/09/21 11:55:10 by ehosta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <curl/curl.h>
+
+char	*get_token(const char *filename)
+{
+	(void)0;
+}
+
+size_t	write_response(void *ptr, size_t size, size_t nmemb, char *data)
+{
+	size_t	realsize;
+
+	(void)data;
+	realsize = size * nmemb;
+	printf("%.*s\n", (int)realsize, (char *)ptr);
+	return (realsize);
+}
+
+struct curl_slist	*prepare_headers(struct curl_slist *headers)
+{
+	headers = curl_slist_append(
+			headers,
+			"Authorization: Bearer 213181908668af1dbb42e43fe72de6bd472018ede7cea0c651fcffa16c975eda"
+			);
+
+	return (headers);
+}
+
+int	main(int argc, char **argv)
+{
+	struct curl_slist	*headers;
+	CURLcode			res;
+	CURL				*curl;
+
+	(void)argc;
+	(void)argv;
+	headers = NULL;
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	curl = curl_easy_init();
+	if (curl)
+	{
+		headers = prepare_headers(headers);
+		curl_easy_setopt(curl, CURLOPT_URL, "https://api.renshuu.org/v1/profile");
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_response);
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		res = curl_easy_perform(curl);
+		if (res != CURLE_OK)
+		{
+			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+		}
+		curl_slist_free_all(headers);
+		curl_easy_cleanup(curl);
+	}
+	curl_global_cleanup();
+	return (0);
+}
